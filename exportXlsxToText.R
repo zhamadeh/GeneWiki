69119b84e4215938cad58b4615b4db41ee16fdd7
zhamadeh <zeid.hamadeh@hotmail.com>
2023-10-17 15:55:31 -0700

    gget strand info

--- exportXlsxToText.R
+++ exportXlsxToText.R
@@ -8,8 +8,12 @@ my_data <- read_excel("Gene Lists/AML.genes.xlsx")
 my_data$Start = prettyNum(my_data$Start, big.mark = ",", scientific = FALSE)
 my_data$End = prettyNum(my_data$End, big.mark = ",", scientific = FALSE)
 
-my_data_svs = filter(my_data,Description=="Structural aberration")
-my_data_genes = filter(my_data,Description!="Structural aberration")
+my_data_genes = my_data
+
+my_data_svs = read_excel("Gene Lists/AML.SVs.xlsx")
+my_data_svs$Start = prettyNum(my_data_svs$Start, big.mark = ",", scientific = FALSE)
+my_data_svs$End = prettyNum(my_data_svs$End, big.mark = ",", scientific = FALSE)
+
 
 write.table(as.data.frame(split(my_data_genes, factor(sort(rank(row.names(my_data_genes))%%2)))[[1]]),
             "Gene Lists/Text/AML.genes.1.txt",quote = F,row.names = F,col.names = F,sep = "\t")
@@ -24,7 +28,7 @@ write.table(my_data_svs,
 my_data <- read_excel("Gene Lists/ALL.genes.xlsx")
 my_data$Start = prettyNum(my_data$Start, big.mark = ",", scientific = FALSE)
 my_data$End = prettyNum(my_data$End, big.mark = ",", scientific = FALSE)
-
+my_data_genes=my_data
 write.table(as.data.frame(split(my_data_genes, factor(sort(rank(row.names(my_data_genes))%%2)))[[1]]),
             "Gene Lists/Text/ALL.genes.1.txt",quote = F,row.names = F,col.names = F,sep = "\t")
 write.table(as.data.frame(split(my_data_genes, factor(sort(rank(row.names(my_data_genes))%%2)))[[2]]),
@@ -33,11 +37,27 @@ write.table(as.data.frame(split(my_data_genes, factor(sort(rank(row.names(my_dat
 
 
 
+allgenes_gtf=rtracklayer::import("Gencode Release 44/gencode.v44.annotation.gtf.gz")
+
 my_data <- read_excel("Gene Lists/AML.ALL.genes.xlsx")
+genes=my_data$`Gene name`
+
+for (gene in genes){
+  if (gene %in% allgenes_gtf@elementMetadata@listData$gene_name){
+    tmp = allgenes_gtf[allgenes_gtf@elementMetadata@listData$gene_name==gene,][1]
+    tmp = as.data.frame(tmp)
+    my_data[my_data$`Gene name`==gene,5][[1]] = tmp$strand
+  } else {
+    message(gene," not found")
+    my_data[my_data$`Gene name`==gene,5][[1]] =  "NA"
+  }
+}
+
+
 my_data$Start = prettyNum(my_data$Start, big.mark = ",", scientific = FALSE)
 my_data$End = prettyNum(my_data$End, big.mark = ",", scientific = FALSE)
-
-write.table(as.data.frame(split(my_data_genes, factor(sort(rank(row.names(my_data_genes))%%2)))[[1]]),
+my_data_genes=my_data
+write.table(as.data.frame(my_data),
             "Gene Lists/Text/AML.ALL.genes.txt",quote = F,row.names = F,col.names = F,sep = "\t")
 
 
